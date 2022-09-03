5b726d01da6f1afc1b42e1781a39d46336036c0f
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-04 03:50:03 +0430

    Added buyTickets component

--- frontend/components/BuyTickets.tsx
+++ frontend/components/BuyTickets.tsx
@@ -4,9 +4,13 @@ import WorldCup from '../assets/WC.png';
 
 const BuyTickets = () => {
   return (
-    <div>
+    <div className='relative text-white'>
         <Image src={WorldCup} width={2000} height={840} />
-        {/* <h1>Buy Tickets</h1> */}
+        <div className='absolute top-1/4 left-1/3'>
+        <h1 className='uppercase text-xl sm:text-2xl lg:text-4xl'>Buy Tickets</h1>
+        <p className=' text-lg sm:text-1xl lg:text-2xl py-5'>Here you can buy tickets in a much more secure <br /> and efficient manner</p>
+        <button className='bg-white py-3 px-5 rounded-full hover:bg-gray-300 text-black text-lg'>Purchase</button>
+        </div>
     </div>
   )
 }
