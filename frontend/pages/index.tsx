353b9e0b36c9bc1e72e0403e355a956138b0c29c
yanukadeneth99 <yanukadeneth99@gmail.com>
2022-09-04 15:29:11 +0530

    Added Nextseo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,24 @@
-import type { NextPage } from 'next'
-import Hero from '../components/Hero'
-import Navbar from '../components/Navbar'
-import Countdown from '../components/Countdown';
-import CallGroups from '../components/CallGroups';
-import BuyTickets from '../components/BuyTickets';
+import type { NextPage } from "next";
+import { NextSeo } from "next-seo";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Countdown from "../components/Countdown";
+import CallGroups from "../components/CallGroups";
+import BuyTickets from "../components/BuyTickets";
 
 const Home: NextPage = () => {
+  return (
+    <>
+      <NextSeo title="NFTicket" description="NFTicket" />
+      <main>
+        <Navbar />
+        <Hero />
+        <Countdown /> {/*Fix this - Hydration */}
+        <CallGroups />
+        <BuyTickets />
+      </main>
+    </>
+  );
+};
 
-  return(
-    <main>
-      <Navbar />
-      <Hero />
-      <Countdown />
-      <CallGroups />
-      <BuyTickets />
-    </main>
-  )
-}
-
-export default Home
+export default Home;
