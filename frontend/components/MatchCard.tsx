1b3621d058f39428282505671a0ce36259b88e27
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-09 09:30:34 +0430

    Added MatchCard Component and Tickets page

---
+++ frontend/components/MatchCard.tsx
@@ -0,0 +1,40 @@
+import React,{ useState } from 'react'
+import Image from 'next/image'
+
+const MatchCard = (props: any): JSX.Element => {
+
+  const { countries }: { countries: { name: string; code: string  }[] } = props
+
+  
+  return (
+    <div className='my-5 bg-[#E5E5E5] py-3 sm:py-10 sm:px-14 w-full sm:rounded-lg h-56 flex flex-col justify-evenly sm:w-2/3 md:m-w-lg lg:max-w-md'>
+      <p className='text-2xl text-center pt-5 pb-3'>21 November 2022</p>
+      {countries.map((country, index) => {
+        return (
+          <div
+            key={index}
+            className="flex flex-row items-end justify-start px-8 py-2 rounded-xl bg-[#ffffff] my-1"
+          >
+            {/* <div className="basis-1/2 w-full flex justify-evenly px-6 py-1"> */}
+              <div className="text-xs relative w-10 h-8 flex flex-col items-end justify-start py-3">
+                <Image
+                  src={`https://countryflagsapi.com/png/${country.code}`}
+                  alt="Flag"
+                  layout="fill"
+                  priority
+                />
+              </div>
+            <h1 className="text-lg py-1 ml-8">{country.name}</h1>
+            {/* </div> */}
+          </div>
+        );
+      })}
+        <div className='text-sm flex justify-between text-[#606C7C] px-2 sm:pt-5 md:pb-3'>
+            <p className='text-center'>Al Bayt Stadium, Al Khor</p>
+            <a href='#' target="_blank" className='text-center'>Match Details</a>
+        </div>
+    </div>
+  )
+}
+
+export default MatchCard
--- frontend/pages/Tickets.tsx
+++ frontend/pages/Tickets.tsx
@@ -1,13 +1,46 @@
 import React from 'react'
+import MatchCard from '../components/MatchCard'
 import Navbar from '../components/Navbar'
 
 const Tickets = () => {
   return (
-    <div className='bg-black min-h-screen'>
+    <div className='bg-black min-h-screen w-full'>
         <Navbar />
-        <div>
+        <div className='lg:px-36'>
             <div className='flex items-center justify-center pt-16'>  
-            <h1 className='text-3xl inline text-white md:text-4xl border-b-2 pb-3'>Matches</h1>
+            <h1 className='text-3xl inline text-white sm:text-4xl border-b-2 pb-3'>Matches</h1>
+            </div>
+            <div className='flex flex-col items-center py-5 md:flex-row md:flex-wrap md:justify-evenly'>
+            <MatchCard 
+            countries={[
+              { name: "Qatar", code: "QT" },
+              { name: "Ecuador", code: "EC" }
+            ]}
+            />
+            <MatchCard 
+            countries={[
+              { name: "England", code: "GB" },
+              { name: "Ecuador", code: "EC" }
+            ]}
+            />
+            <MatchCard 
+            countries={[
+              { name: "England", code: "GB" },
+              { name: "Ecuador", code: "EC" }
+            ]}
+            />
+            <MatchCard 
+            countries={[
+              { name: "England", code: "GB" },
+              { name: "Ecuador", code: "EC" }
+            ]}
+            />
+            <MatchCard 
+            countries={[
+              { name: "England", code: "GB" },
+              { name: "Ecuador", code: "EC" }
+            ]}
+            />
             </div>
         </div>
     </div>
