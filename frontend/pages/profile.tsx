5ee7873bc635d7f829074a7d514eefbd8244857d
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-17 11:57:06 +0430

    Profile page wrapped up

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import TicketCard from '../components/TicketCard';
-import Footer from '../components/Footer';
 
 const profile = () => {
   return (
