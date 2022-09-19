8b2e9b43fff78ca034ad96b527c6d92f82b1d9ef
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-09-19 11:44:55 +0430

    Added image for when user has no tickets

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
 import TicketCard from '../components/TicketCard';
+import Image from 'next/image';
+import empty from '../public/img/empty.png';
 
 const profile = () => {
+  const [ticketPurchased, setTicketPurchased] = useState<boolean>(false);
+  
   return (
     <div className='bg-black min-h-screen'>
         <Navbar />
         <div className='text-white flex justify-around flex-wrap gap-y-20 gap-x-14'>
-        <TicketCard />
-        <TicketCard />
+        {ticketPurchased ? <div>
+          <TicketCard />
+          <TicketCard />
+        </div> : (
+        <div className='pt-32'>
+        <Image src={empty} height={300} width={250} />
+        <h1 className='py-10 text-center'>You don't own any tickets....</h1>
+        </div>)}
         </div>
     </div>
   )
