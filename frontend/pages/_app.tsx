54db334f31f9f9a110aaae3a009c6a52cdaff7db
Larry <user@Users-MacBook-Pro.local>
2022-09-20 13:12:35 -0500

    added buyTicket smart contract functionality

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,13 +6,47 @@ import {
   RainbowKitProvider,
   lightTheme,
 } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { Chain, chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
+const scaleChain: Chain = {
+  id: 256236330,
+  name: "Skale-Live-Vega-Chain",
+  network: "skale",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Skale Fuel",
+    symbol: "sFUEL",
+  },
+  rpcUrls: {
+    default: "https://eth-online.skalenodes.com/v1/hackathon-content-live-vega",
+  },
+  blockExplorers: {
+    default: {
+      name: "Skale Explorer",
+      url: "https://hackathon-content-live-vega.explorer.eth-online.skalenodes.com/",
+    },
+  },
+  testnet: true,
+};
+
+// const { chains, provider } = configureChains(
+//   [chain.goerli],
+//   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+// );
+
 const { chains, provider } = configureChains(
-  [chain.goerli],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+  [scaleChain],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => {
+        if (chain.id !== scaleChain.id) return null;
+        return { http: chain.rpcUrls.default };
+      },
+    }),
+  ]
 );
 
 const { connectors } = getDefaultWallets({
@@ -28,20 +62,20 @@ const wagmiClient = createClient({
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        chains={chains}
-        modalSize="compact"
-        theme={lightTheme({
-          accentColor: "whitesmoke",
-          accentColorForeground: "black",
-          borderRadius: "small",
-          fontStack: "rounded",
-        })}
-      >
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          chains={chains}
+          modalSize="compact"
+          theme={lightTheme({
+            accentColor: "whitesmoke",
+            accentColorForeground: "black",
+            borderRadius: "small",
+            fontStack: "rounded",
+          })}
+        >
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
   );
 }
 
