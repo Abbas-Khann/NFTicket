9acd230420bb7d3bfc7e82929f0908e0d37918e2
yanukadeneth99 <yanukadeneth99@gmail.com>
2022-09-21 17:53:23 +0530

    Squashed commit of the following:
    commit c88872df738f6aec24e5a2a7f156282fdc697e16
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 16:47:58 2022 +0430
    
        Finished Reselling design
    
    commit b1a540b2b5c336033aa219ad0c2ab1a374406308
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 16:01:45 2022 +0430
    
        Added resell page
    
    commit 1be4353e9a42837c45eb8facec63a1879eed97b0
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 16:00:35 2022 +0430
    
        Wrapped up the Graph component
    
    commit 91ce5afe7027279130d6a4e1a3a394d3f83b0691
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 14:29:31 2022 +0430
    
        Added new pages to Navbar
    
    commit 464fbe0ccd4f092968939b087dcdd302c08809c6
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 14:23:55 2022 +0430
    
        Removed fullscreen option from embed
    
    commit 65cce319c23cbbfadb9ca2fd8f7e3656cecfa204
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 14:20:09 2022 +0430
    
        Graph table completed
    
    commit 83f33802b04af8f2559ed4fa4efbfd8b14407d9d
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 14:09:57 2022 +0430
    
        Added graph activity
    
    commit edfe4e49c53cb60c5ffcc4fdb286899aa2ac8f4a
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 12:02:35 2022 +0430
    
        Activity page created
    
    commit 19eebd58b5f0a944c13af0d8d0b5c7e29a28e101
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 11:30:32 2022 +0430
    
        Finished Usage page
    
    commit 2e8209aa1a3fed12f61eb507957a399fd1602709
    Author: Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
    Date:   Wed Sep 21 10:10:12 2022 +0430
    
        Fixed color issue

---
+++ frontend/pages/Usage.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import image from '../public/img/WC.png';
+import Image from 'next/image';
+import Footer from '../components/Footer';
+
+const Usage = () => {
+  return (
+    <div>
+        <Navbar />
+        <div className='bg-[#07031A] min-h-screen text-white'>
+            <h1 className='text-4xl md:text-5xl text-center py-14 md:py-20'>How To Use?</h1>
+                <h1 className='text-4xl text-center py-10'>Step One</h1>
+            <div className='flex sm:px-24 md:px-32 lg:px-36'>
+                <div className='w-10/12 sm:w-6/12 flex flex-col justify-center lg:pl-32'>
+                    <h2 className='text-3xl pb-8'>Connect your Wallet</h2>
+                    <p className='text-xl'>
+                        Lorem ipsum dolor sit amet consectetur, <br />adipisicing elit. Nam quibusdam <br /> explicabo assumenda.
+                    </p>
+                </div>
+                <div className='flex flex-col items-center justify-center w-6/12'>
+                <Image
+                  src={image}
+                  className="rounded-full object-cover"
+                  width={300}
+                  height={300}
+                />
+                </div>
+            </div>
+                <h1 className='text-4xl text-center pt-32 pb-10'>Step Two</h1>
+            <div className='flex sm:px-24 md:px-32 lg:px-36'>
+                <div className='flex flex-col items-center justify-center w-6/12'>
+                <Image
+                  src={image}
+                  className="rounded-full object-cover"
+                  width={300}
+                  height={300}
+                />
+                </div>
+                <div className='w-10/12 sm:w-6/12 flex flex-col justify-center lg:pl-32'>
+                    <h2 className='text-3xl pb-8'>Connect your Wallet</h2>
+                    <p className='text-xl'>
+                        Lorem ipsum dolor sit amet consectetur, <br />adipisicing elit. Nam quibusdam <br /> explicabo assumenda.
+                    </p>
+                </div>
+            </div>
+                <h1 className='text-4xl text-center pt-32 pb-10'>Step Three</h1>
+            <div className='flex sm:px-24 md:px-32 lg:px-36'>
+                <div className='w-10/12 sm:w-6/12 flex flex-col justify-center lg:pl-32'>
+                    <h2 className='text-3xl pb-8'>Connect your Wallet</h2>
+                    <p className='text-xl'>
+                        Lorem ipsum dolor sit amet consectetur, <br />adipisicing elit. Nam quibusdam <br /> explicabo assumenda.
+                    </p>
+                </div>
+                <div className='flex flex-col items-center justify-center w-6/12'>
+                <Image
+                  src={image}
+                  className="rounded-full object-cover"
+                  width={300}
+                  height={300}
+                />
+                </div>
+            </div>
+            <div className='flex flex-col items-center py-24'>
+            <iframe width="760" height="455" src="https://www.youtube.com/embed/fs5Xp1P8Gck" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            </div>
+            <Footer />
+        </div>
+    </div>
+  )
+}
+
+export default Usage
